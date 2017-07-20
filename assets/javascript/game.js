5e2c4a85bd52872d50556b3d63288e456c046342
Samantha Roseman <samantharoseman@Sams-MacBook-Pro.local>
2017-07-19 21:01:36 -0700

    wednesday night progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,107 @@
+		var user;
+		var comp;
+		var math;
+		var wins = 0;
+		var losses = 0;
+		var guessesLeft = 10;
+		var words = ["cats","claw","meow","paws"];
+		var compAnswer = document.getElementById("comp-text");
+		var userA = document.getElementById("user-a");
+		var userB = document.getElementById("user-b");
+		var userC = document.getElementById("user-c");
+		var userD = document.getElementById("user-d");
+		var userGuess = document.getElementById("user-text");
+		var winsText = document.getElementById("wins-text");
+		var lossesText = document.getElementById("losses-text");
+		var guessesText = document.getElementById("guesses-text");
+		var userArray = [];
+		var idx;
+
+
+		// creating a function that chooses a random string from my words array
+		function getWord() {
+			math = Math.floor(Math.random() * words.length);
+			comp = words[math];
+			// console.log(comp);
+		}
+
+		// start over function
+		function start() {
+			guessesLeft = 10;
+			getWord();
+			userArray = [];	
+			userA.textContent = " _ "
+			userB.textContent = " _ "	
+			userC.textContent = " _ "	
+			userD.textContent = " _ "			
+		}
+
+		// call the start function
+		start();
+		// sets comp to the word the random generator picked
+		
+		
+		
+
+
+		// listens for user key press
+		document.onkeyup = function(push) {
+		
+		// set variable use to whatever key the user pressed	
+		var user = push.key;
+
+		// set idx to the index of the user's key in the comps string
+			idx = comp.indexOf(user) 
+				if (idx !== -1) {
+				
+					if (idx === 0){
+						userA.textContent = user;
+					}
+					else if (idx === 1){
+						
+						userB.textContent = user;
+					}
+					else if (idx === 2){
+						
+						userC.textContent = user;
+					}
+					else if (idx === 3){
+						
+						userD.textContent = user;
+					}
+
+				}
+				else {
+					guessesLeft--
+					userArray.push(" " + user);
+					// console.log("Guesses:" + userArray);
+					// console.log("Guesses Left:" + guessesLeft);
+				}
+
+
+				if (guessesLeft === 0) {
+					losses++;
+					alert("You lost :(");
+					start();
+
+				}
+				
+				if (userA.textContent !== " _ " && userB.textContent !== " _ " && userC.textContent !== " _ " && userD.textContent !== " _ "){
+					wins++;
+					alert("winner");
+					start();
+				}
+
+
+				userGuess.textContent = userArray;
+				guessesText.textContent = guessesLeft;
+				winsText.textContent = wins;
+				lossesText.textContent = losses;
+
+
+			
+	
+
+		};
+
+	
